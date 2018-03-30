2ec72462d9038160f9c54ceecf06d409a74dbb13
Faithlmy <lmengyy@126.com>
2018-03-30 20:34:27 +0800

    Generic

---
+++ src/GenericParadigm/BeforeGeneric.java
@@ -0,0 +1,28 @@
+package GenericParadigm;
+
+public class BeforeGeneric {
+	static class ArrayList{
+		private Object[] elements = new Object[0];
+		public Object get(int i) {
+			return elements[0];
+		}
+		
+		public  void add(Object o) {
+			int length = elements.length;
+			Object[] newElements = new Object[length + 1];
+			for (int i = 0; i < length; i++) {
+				newElements[i] = elements[i];
+			}
+		newElements[length] = 0;
+		elements = newElements;
+		}
+	}
+	
+	public static void main(String[] args) {
+		ArrayList stringValues=new ArrayList();
+		stringValues.add(1);
+		String str=(String) stringValues.get(0);
+		
+	}
+
+}
