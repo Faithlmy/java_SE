88f582e3aa5630290cec88c22c1e4be0dbfc1278
Faithlmy <lmengyy@126.com>
2018-04-03 16:05:16 +0800

    add Object Class

---
+++ src/ObjectClass/User.java
@@ -0,0 +1,34 @@
+package ObjectClass;
+
+public class User {
+	private int uid;
+	private String name;
+	private int age;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public boolean equals(Object obj) {
+		if(obj == null || !(obj instanceof User))
+			return false;
+		if(((User) obj).getUid() == this.getUid())
+			return true;
+		return false;
+	}
+
+}
---
+++ src/ObjectClass/UserMain.java
@@ -0,0 +1,18 @@
+package ObjectClass;
+
+public class UserMain implements Cloneable{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		User u1 = new User();
+		u1.setUid(121);
+		u1.setName("feng");
+		
+		User u2 = new User();
+		u2.setUid(121);
+		u2.setName("faith");
+		
+		System.out.println(u1.equals(u2));// the result is true.  User类定义中重写了equals()方法
+	}
+
+}
